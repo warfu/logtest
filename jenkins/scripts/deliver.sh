161dc21a5fb44a1e1f046b9da02be8aa5e39406d
wangf13 <wangf13@mingyuanyun.com>
2018-08-11 23:13:52 +0800

    更新代码 创建docker

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -27,13 +27,16 @@ set -x
 
 
 echo "删除 Dockerfile"
-rm -f ${WORKSPACE}/Dockerfile
+
+dockerFile= ${WORKSPACE}/Dockerfile
+
+rm -f ../Dockerfile
 
 
-echo   "FROM frolvlad/alpine-oraclejdk8:slim" >> ${WORKSPACE}/Dockerfile
-echo   "VOLUME /tmp" >> ../Dockerfile
-echo   "ADD  $PWD/target/${NAME}-${VERSION}.jar  app.jar"  >> ${WORKSPACE}/Dockerfile
-echo   'ENTRYPOINT ["java","-jar","/app.jar"]' >> ${WORKSPACE}/ Dockerfile
+echo   "FROM frolvlad/alpine-oraclejdk8:slim" >> ../Dockerfile
+echo   "VOLUME /tmp" >>  ../Dockerfile
+echo   "ADD  target/${NAME}-${VERSION}.jar  app.jar"  >> ../Dockerfile
+echo   'ENTRYPOINT ["java","-jar","/app.jar"]' >> ../Dockerfile
 
 echo "生成 Dockerfile"
 
