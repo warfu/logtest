b468d22c918966add9f93056e457df9fe5980bbc
wangf13 <wangf13@mingyuanyun.com>
2018-08-11 18:57:25 +0800

    更新代码 创建docker

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,3 +24,32 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 #java -jar target/${NAME}-${VERSION}.jar --server.port=8081
+
+
+echo "删除 Dockerfile"
+rm -f Dockerfile
+
+
+echo   "FROM frolvlad/alpine-oraclejdk8:slim" >> Dockerfile
+echo   "VOLUME /tmp" >> Dockerfile
+echo   "ADD target/${NAME}-${VERSION}.jar app.jar"  >> Dockerfile
+echo   'ENTRYPOINT ["java","-jar","/app.jar"]' >> Dockerfile
+
+echo "生成 Dockerfile"
+
+
+echo "开始 构建镜像  solcloud "
+#构建镜像
+docker build -t solcloud .
+
+echo " 镜像 solcloud 构建完成"
+
+echo " 开始启动 容器 solcloud"
+#启动容器
+docker run -d --name solcloud  -p 9080:8080 solcloud:latest
+
+echo "  容器 solcloud 启动完成"
+
+
+
+
