4c73cc808b2e39582e4d993ecdce759e2ed8748c
wangf13 <wangf13@mingyuanyun.com>
2018-08-11 15:02:28 +0800

    first commit

---
+++ src/main/java/com/mysoft/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.mysoft.web;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(@RequestParam String name) {
+        return "Hello " + name;
+    }
+
+}
